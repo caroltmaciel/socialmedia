ee52dfa1558da5ee50b1ff681ce0e91bc5c115b3
Ana Carolina <anacarolinamaciel15@gmail.com>
2025-09-30 10:53:51 +0100

    Inserção de usuário com POST

--- src/main/java/br/com/github/caroltmaciel/socialmedia/services/UserService.java
+++ src/main/java/br/com/github/caroltmaciel/socialmedia/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.github.caroltmaciel.socialmedia.services;
 
 import br.com.github.caroltmaciel.socialmedia.domain.User;
+import br.com.github.caroltmaciel.socialmedia.dto.UserDto;
 import br.com.github.caroltmaciel.socialmedia.repository.UserRepository;
 import br.com.github.caroltmaciel.socialmedia.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id) {
         return repo.findById(id)
             .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDto objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
